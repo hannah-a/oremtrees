492c0368ff7702cac463d23c1bcba2b180ed1ac3
hannah-a <hannahlinarmstrong@gmail.com>
2024-10-26 15:02:31 -0600

    custom css injector

---
+++ wp-content/uploads/custom-code-injector/about-script.js
@@ -0,0 +1,2 @@
+console.log('About Page')
+console.log('test')
---
+++ wp-content/uploads/custom-css-js/53.js
@@ -0,0 +1,4 @@
+<!-- start Simple Custom CSS and JS -->
+<script type="text/javascript">
+alert('link!')</script>
+<!-- end Simple Custom CSS and JS -->
