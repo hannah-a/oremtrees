2c31dfaf4ca4a58bbc0f820ee05026388e172b4b
Hannah Armstrong <hannahlinarmstrong@gmail.com>
2024-11-01 12:42:29 -0600

    created styles on the copyright block

--- wp-content/plugins/copyright-date-block/build/index.asset.php
+++ wp-content/plugins/copyright-date-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'd33323727f64b32e1dc8');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'bf2c6543e4b178c9353d');
