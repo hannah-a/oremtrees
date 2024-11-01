8c5f473ece84a15d7ee6658e08b50a2e2340b6b5
Hannah Armstrong <hannahlinarmstrong@gmail.com>
2024-11-01 13:58:27 -0600

    made block with grey and no border, now showing dotted on editor

--- wp-content/plugins/copyright-date-block/build/index.asset.php
+++ wp-content/plugins/copyright-date-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'bf2c6543e4b178c9353d');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'aca465bc2521407169bb');
--- wp-content/plugins/copyright-date-block/src/edit.js
+++ wp-content/plugins/copyright-date-block/src/edit.js
@@ -54,6 +54,7 @@ export default function Edit( { attributes, setAttributes } ) {
 			) }
 		 &copy; { startingYear } - { currentYear }
 		</p>
+	
 		</>
 	);
 }
